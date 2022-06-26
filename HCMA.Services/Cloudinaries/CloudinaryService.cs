55462e1ee88ae0924b14bab768b20132e7da7c70
nika19du <qnkovanikol29@gmail.com>
2022-06-26 22:25:55 +0300

    started work on employeeService implementation

---
+++ HCMA.Services/Cloudinaries/CloudinaryService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using System.IO;
+using CloudinaryDotNet.Actions;
+
+namespace HCMA.Services.Cloudinaries
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+
+        private readonly Cloudinary cloudinaryUtility;
+
+        public CloudinaryService(Cloudinary cloudinaryUtility)
+        {
+            this.cloudinaryUtility = cloudinaryUtility;
+        }
+
+        public async Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName)
+        {
+            if (pictureFile == null) return null;
+            byte[] destinationData;
+
+            using (var ms = new MemoryStream())
+            {
+                await pictureFile.CopyToAsync(ms);
+                destinationData = ms.ToArray();
+            }
+
+            UploadResult uploadResult = null;
+
+            using (var ms = new MemoryStream(destinationData))
+            {
+                ImageUploadParams uploadParams = new ImageUploadParams
+                {
+                    Folder = "HCMA_images",
+                    File = new FileDescription(fileName, ms)
+                };
+
+                uploadResult = this.cloudinaryUtility.Upload(uploadParams);
+            }
+            return uploadResult?.SecureUri.AbsoluteUri;
+        }
+    }
+}
---
+++ HCMA.Services/Cloudinaries/ICloudinaryService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCMA.Services.Cloudinaries
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadPictureAsync(IFormFile pictureFile, string fileName);
+    }
+}
